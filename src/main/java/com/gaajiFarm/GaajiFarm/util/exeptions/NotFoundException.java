1f4d4e9fbfa5e72d4ad18567dd10a74c7e281898
gaji12345 <gajimax12345@gmail.com>
2024-10-26 20:45:58 +0530

    create custom exeption class

---
+++ src/main/java/com/gaajiFarm/GaajiFarm/util/exeptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.gaajiFarm.GaajiFarm.util.exeptions;/*  gaajiCode
+    99
+    09/09/2024
+    */
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
