120626b387d9789a9c1b019151577d2660c6ce63
gaji12345 <gajimax12345@gmail.com>
2024-10-26 19:58:39 +0530

    test

---
+++ src/main/java/com/gaajiFarm/GaajiFarm/GaajiFarmApplication.java
@@ -0,0 +1,13 @@
+package com.gaajiFarm.GaajiFarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaajiFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaajiFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaajiFarm/GaajiFarm/GaajiFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaajiFarm.GaajiFarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaajiFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
