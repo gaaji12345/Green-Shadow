4cc9e04d5b1b0042bdb9a8c74a7c00686222150f
gaji12345 <gajimax12345@gmail.com>
2024-10-26 20:48:17 +0530

    JwtSerives class are done

---
+++ src/main/java/com/gaajiFarm/GaajiFarm/config/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.gaajiFarm.GaajiFarm.config;/*  gaajiCode
+    99
+    09/09/2024
+    */
+
+
+
+import com.gaajiFarm.GaajiFarm.util.enitity.User;
+import com.gaajiFarm.GaajiFarm.util.exeptions.NotFoundException;
+import com.gaajiFarm.GaajiFarm.util.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email Not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
